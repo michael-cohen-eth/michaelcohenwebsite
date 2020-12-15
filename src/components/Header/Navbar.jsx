d95e194410d75d99f05508331aca999f4924b8fb
Michael Cohen <micohen13@gmail.com>
2020-12-15 00:23:28 -0500

    easier branching logic for services-section

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -30,9 +30,6 @@ const Navigation = ({ navigationData, sticky = false }) => {
 
   const navPrefix = 'nav-item-';
   const listBuilder = headers.map((headerText) => {
-    if (process.env.BRANCH !== 'services-section' && headerText === 'services') {
-      return <span key={`${navPrefix}${headerText}`} />;
-    }
     return (
       <li className="nav-item col" key={`${navPrefix}${headerText}`}>
         <Link
