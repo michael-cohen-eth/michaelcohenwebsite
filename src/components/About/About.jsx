b73c2bb172adbe9775c30f62f0cb4978649dae25
Michael Cohen <micohen13@gmail.com>
2020-12-14 19:15:22 -0500

    added about from firebase

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,15 +1,14 @@
-import React, { useContext, useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import Fade from 'react-reveal/Fade';
+import firebase from 'gatsby-plugin-firebase';
 import { Container, Row, Col } from 'react-bootstrap';
 import { Typography } from '@material-ui/core';
-import { useStaticQuery, graphql } from "gatsby";
+import { useStaticQuery, graphql } from 'gatsby';
 import Title from '../Title/Title';
 import AboutImg from '../Image/AboutImg';
-import PortfolioContext from '../../context/context';
+import Loading from '../Utils/Loading';
 
 const About = ({ stickyAnchor }) => {
-  const { about } = useContext(PortfolioContext);
-  const { img, paragraphOne, paragraphTwo, paragraphThree } = about;
   const resumeInfo = useStaticQuery(graphql`
     {
       allFile(filter: { extension: { eq: "pdf" } }) {
@@ -22,6 +21,8 @@ const About = ({ stickyAnchor }) => {
       }
     }
   `).allFile.edges[0];
+  const [about, setAbout] = useState({});
+  const [loading, setLoading] = useState(true);
   const [isDesktop, setIsDesktop] = useState(false);
   const [isMobile, setIsMobile] = useState(false);
 
@@ -33,51 +34,65 @@ const About = ({ stickyAnchor }) => {
       setIsMobile(true);
       setIsDesktop(false);
     }
+    firebase
+      .firestore()
+      .collection('portfolio')
+      .doc('about')
+      .get()
+      .then((snapshot) => {
+        if (snapshot.exists) {
+          setAbout(snapshot.data());
+          setLoading(false);
+        }
+      });
   }, []);
 
   return (
     <section id="about">
       <Container>
         <Title title="About Me" />
-        <Row className="about-wrapper">
-          <Col md={6} sm={12}>
-            <Fade bottom duration={1000} delay={600} distance="30px">
-              <div className="about-wrapper__image">
-                <AboutImg alt="profile picture" filename={img} />
-              </div>
-            </Fade>
-          </Col>
-          <Col md={6} sm={12}>
-            <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
-              <div className="about-wrapper__info">
-                <Typography variant="h5" className="about-wrapper__info-text">
-                  {paragraphOne}
-                </Typography>
-                <Typography variant="h5" className="about-wrapper__info-text">
-                  {paragraphTwo}
-                </Typography>
-                <Typography variant="h5" className="about-wrapper__info-text">
-                  {paragraphThree}
-                </Typography>
-                <br />
-                <br />
-                <Typography variant="h5" className="about-wrapper__info-text">
-                  are you more oldschool?
-                </Typography>
-                <span className="d-flex mt-3">
-                  <a
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="cta-btn cta-btn--resume"
-                    href={resumeInfo.node.publicURL}
-                  >
-                    my resume.pdf
-                  </a>
-                </span>
-              </div>
-            </Fade>
-          </Col>
-        </Row>
+        {loading && <Loading />}
+        {!loading && (
+          <Row className="about-wrapper">
+            <Col md={6} sm={12}>
+              <Fade bottom duration={1000} delay={600} distance="30px">
+                <div className="about-wrapper__image">
+                  <AboutImg alt="profile picture" filename={about.img} />
+                </div>
+              </Fade>
+            </Col>
+            <Col md={6} sm={12}>
+              <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
+                <div className="about-wrapper__info">
+                  <Typography variant="h5" className="about-wrapper__info-text">
+                    {about.paragraphOne}
+                  </Typography>
+                  <Typography variant="h5" className="about-wrapper__info-text">
+                    {about.paragraphTwo}
+                  </Typography>
+                  <Typography variant="h5" className="about-wrapper__info-text">
+                    {about.paragraphThree}
+                  </Typography>
+                  <br />
+                  <br />
+                  <Typography variant="h5" className="about-wrapper__info-text">
+                    are you more oldschool?
+                  </Typography>
+                  <span className="d-flex mt-3">
+                    <a
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="cta-btn cta-btn--resume"
+                      href={resumeInfo.node.publicURL}
+                    >
+                      my resume.pdf
+                    </a>
+                  </span>
+                </div>
+              </Fade>
+            </Col>
+          </Row>
+        )}
       </Container>
     </section>
   );
